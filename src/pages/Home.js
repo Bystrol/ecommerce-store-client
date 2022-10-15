776a33fc04fd486d5e66060240148898f5ccadc5
Michał <michalbystryk14@wp.pl>
2022-10-15 16:17:51 +0200

    styled home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import classes from "./Home.module.css";
 
 const Home = () => {
-  return <div className={classes.body}></div>;
+  return (
+    <div className={classes.body}>
+      <p>Your favourite online clothing shop.</p>
+      <div className={classes.layer} />
+      <div className={classes.background} />
+    </div>
+  );
 };
 
 export default Home;
