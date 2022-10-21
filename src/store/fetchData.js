bd019926f05c0dd8b41d482f62b341d896282f9d
Michał <michalbystryk14@wp.pl>
2022-10-21 17:53:53 +0200

    changed api url, added error catch/detail fetching

--- src/store/fetchData.js
+++ src/store/fetchData.js
@@ -1,10 +1,11 @@
 import { categoryActions } from "./categorySlice";
+import { detailActions } from "./detailSlice";
 
-export const fetchData = () => {
+export const fetchCategoryData = (category) => {
   return async (dispatch) => {
     try {
       const response = await fetch(
-        "https://online-store-b05f3-default-rtdb.firebaseio.com/women.json"
+        `https://online-store-b05f3-default-rtdb.firebaseio.com/${category}.json`
       );
 
       if (!response.ok) {
@@ -14,6 +15,34 @@ export const fetchData = () => {
       const data = await response.json();
 
       dispatch(categoryActions.fetchData(data || []));
-    } catch (error) {}
+    } catch (error) {
+      alert(
+        "Could not fetch the data! Make sure that you've entered the API URL correctly."
+      );
+    }
+  };
+};
+
+export const fetchDetailData = (category, id) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        `https://online-store-b05f3-default-rtdb.firebaseio.com/${category}.json`
+      );
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const data = await response.json();
+
+      const detailItem = await data.filter((item) => item.id === id);
+
+      dispatch(detailActions.setItem(detailItem));
+    } catch (error) {
+      alert(
+        "Could not fetch the data! Make sure that you've entered the API URL correctly."
+      );
+    }
   };
 };
