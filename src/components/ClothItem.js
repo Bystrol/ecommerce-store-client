909799b7a9203c88ff659c8bac3335f14ebaf3f6
Michał <michalbystryk14@wp.pl>
2022-10-21 21:47:47 +0200

    added Price component

--- src/components/ClothItem.js
+++ src/components/ClothItem.js
@@ -1,15 +1,41 @@
 import classes from "./ClothItem.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate, useParams } from "react-router";
+import { useSelector } from "react-redux/es/exports";
 
 const ClothItem = (props) => {
   const navigate = useNavigate();
   const category = useParams("category").category;
-
+  const currency = useSelector((state) => state.currency.currency);
   const isAvailable = props.isAvailable;
 
   const openDetailHandler = () => {
     navigate(`/${category}/${props.id}`);
+  };
+
+  const Price = () => {
+    if (currency === "EUR") {
+      return (
+        <>
+          <FontAwesomeIcon icon="fa-euro-sign" />
+          {(props.price * 1.025).toFixed(2)}
+        </>
+      );
+    } else if (currency === "GBP") {
+      return (
+        <>
+          <FontAwesomeIcon icon="fa-sterling-sign" />
+          {(props.price * 0.8985).toFixed(2)}
+        </>
+      );
+    }
+
+    return (
+      <>
+        <FontAwesomeIcon icon="fa-dollar-sign" />
+        {props.price}
+      </>
+    );
   };
 
   return (
@@ -29,7 +55,7 @@ const ClothItem = (props) => {
         <FontAwesomeIcon icon="cart-shopping" className={classes.cart} />
       </div>
       <p className={classes.name}>{props.name}</p>
-      <p className={classes.price}>{props.price}</p>
+      <p className={classes.price}>{<Price />}</p>
     </div>
   );
 };
