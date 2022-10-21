37708940d4395865d614daded392900ff5aa9854
Michał <michalbystryk14@wp.pl>
2022-10-21 21:52:08 +0200

    added currencySlice reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categorySlice from "./categorySlice";
 import detailSlice from "./detailSlice";
+import currencySlice from "./currencySlice";
 
 const store = configureStore({
-  reducer: { category: categorySlice.reducer, detail: detailSlice.reducer },
+  reducer: {
+    category: categorySlice.reducer,
+    detail: detailSlice.reducer,
+    currency: currencySlice.reducer,
+  },
 });
 
 export default store;
