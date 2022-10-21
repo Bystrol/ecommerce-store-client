9b9f9e84a0ed9ffa3e70f423c52638369a72637b
Michał <michalbystryk14@wp.pl>
2022-10-21 17:50:25 +0200

    created Detail.js

---
+++ src/pages/Detail.js
@@ -0,0 +1,37 @@
+import { fetchDetailData } from "../store/fetchData";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import ItemDetail from "../components/ItemDetail";
+
+const Detail = () => {
+  const itemId = useParams("id").id;
+  const category = useParams("category").category;
+
+  const clickedItem = useSelector((state) => state.detail.item);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDetailData(category, itemId));
+  }, []);
+
+  return (
+    <>
+      {clickedItem.map((item) => {
+        return (
+          <ItemDetail
+            key={item.id}
+            id={item.id}
+            imageUrl={item.imageUrl}
+            name={item.name}
+            price={item.price}
+            description={item.description}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default Detail;
