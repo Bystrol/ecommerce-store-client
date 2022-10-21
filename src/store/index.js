69967a5e50da2a015d1a8b77f6a761c0851eecf8
Michał <michalbystryk14@wp.pl>
2022-10-21 17:54:23 +0200

    added detailSlice reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categorySlice from "./categorySlice";
+import detailSlice from "./detailSlice";
 
-const store = configureStore({ reducer: { category: categorySlice.reducer } });
+const store = configureStore({
+  reducer: { category: categorySlice.reducer, detail: detailSlice.reducer },
+});
 
 export default store;
