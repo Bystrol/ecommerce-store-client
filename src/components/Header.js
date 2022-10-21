557d2f4d4a45fd8e965b0b3e7be6aee4d1d25700
Michał <michalbystryk14@wp.pl>
2022-10-21 21:48:40 +0200

    added Sign component and currency list

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,40 @@
 import classes from "./Header.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { currencyActions } from "../store/currencySlice";
 
 const Header = (props) => {
+  const [showCurrencyList, setShowCurrencyList] = useState(false);
+  const currency = useSelector((state) => state.currency.currency);
+  const dispatch = useDispatch();
+
+  const toggleCurrencyListHandler = () => {
+    setShowCurrencyList((state) => {
+      return !state;
+    });
+  };
+
+  const hideCurrencyListHandler = () => {
+    setShowCurrencyList(false);
+  };
+
+  const setCurrencyHandler = (e) => {
+    dispatch(currencyActions.setCurrency(e.target.textContent));
+    setShowCurrencyList(false);
+  };
+
+  const Sign = () => {
+    if (currency === "EUR") {
+      return <FontAwesomeIcon icon="fa-euro-sign" />;
+    } else if (currency === "GBP") {
+      return <FontAwesomeIcon icon="fa-sterling-sign" />;
+    }
+
+    return <FontAwesomeIcon icon="fa-dollar-sign" />;
+  };
+
   return (
     <>
       <div className={classes.header}>
@@ -37,14 +68,35 @@ const Header = (props) => {
           <FontAwesomeIcon icon="fa-shirt" className={classes.logo} />
         </Link>
         <div className={classes.payment}>
-          <p>
-            <FontAwesomeIcon icon="fa-dollar-sign" className={classes.dollar} />
+          <div className={classes.currency} onClick={toggleCurrencyListHandler}>
+            {<Sign />}
             <FontAwesomeIcon icon="angle-down" className={classes.down} />
-          </p>
+          </div>
           <FontAwesomeIcon icon="cart-shopping" className={classes.cart} />
         </div>
+        {showCurrencyList && (
+          <ul className={classes.container}>
+            <li onClick={setCurrencyHandler}>
+              <FontAwesomeIcon icon="fa-dollar-sign" className={classes.sign} />
+              USD
+            </li>
+            <li onClick={setCurrencyHandler}>
+              <FontAwesomeIcon icon="fa-euro-sign" className={classes.sign} />
+              EUR
+            </li>
+            <li onClick={setCurrencyHandler}>
+              <FontAwesomeIcon
+                icon="fa-sterling-sign"
+                className={classes.sign}
+              />
+              GBP
+            </li>
+          </ul>
+        )}
       </div>
-      <main className={classes.main}>{props.children}</main>
+      <main className={classes.main} onClick={hideCurrencyListHandler}>
+        {props.children}
+      </main>
     </>
   );
 };
