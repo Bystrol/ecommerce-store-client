fbcde6b7e9a8c25b5c80dbbbf946a0be4665a6ea
Michał <michalbystryk14@wp.pl>
2022-10-29 11:27:17 +0200

    added cartSlice reducer

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import categorySlice from "./categorySlice";
 import detailSlice from "./detailSlice";
 import currencySlice from "./currencySlice";
+import cartSlice from "./cartSlice";
 
 const store = configureStore({
   reducer: {
     category: categorySlice.reducer,
     detail: detailSlice.reducer,
     currency: currencySlice.reducer,
+    cart: cartSlice.reducer,
   },
 });
 
