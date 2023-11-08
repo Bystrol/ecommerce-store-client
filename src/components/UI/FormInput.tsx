c58171b87d0c02b54dfb9a9bf709ae371bac0622
Michal <michalbystryk14@wp.pl>
2023-11-08 17:38:26 +0100

    Add FormInput component.

---
+++ src/components/UI/FormInput.tsx
@@ -0,0 +1,38 @@
+import styles from "./FormInput.module.css"
+
+type FormInputProps = {
+  name: string
+  labelText: string
+  type: string
+  isInvalid: boolean
+  validationText: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onBlur: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const FormInput = (props: FormInputProps) => {
+  return (
+    <>
+      <label htmlFor={props.name} className={styles.label}>
+        {props.labelText}
+      </label>
+      <input
+        type={props.type}
+        name={props.name}
+        id={props.name}
+        className={
+          props.isInvalid
+            ? `${styles.input} ${styles["input--invalid"]}`
+            : styles.input
+        }
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+      {props.isInvalid ? (
+        <p className={styles.p}>{props.validationText}</p>
+      ) : null}
+    </>
+  )
+}
+
+export default FormInput
