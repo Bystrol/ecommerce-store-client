fff82e5b1b1d2a10775816db17f3f893c74a697e
Michal <michalbystryk14@wp.pl>
2023-11-09 20:13:34 +0100

    Add toast notifications to register function

--- src/util/api/register.ts
+++ src/util/api/register.ts
@@ -1,3 +1,5 @@
+import toast from "react-hot-toast"
+
 type RegisterParams = {
   username: string
   email: string
@@ -6,22 +8,27 @@ type RegisterParams = {
 }
 
 export const register = async (params: RegisterParams) => {
-  try {
-    const response = await fetch(
-      `${process.env.REACT_APP_API_URL}/auth/register`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(params),
-      }
-    )
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/auth/register`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    }
+  )
 
-    const data: { message: string; authToken: string } = await response.json()
+  const data: { message: string } = await response.json()
 
-    return data
-  } catch (err) {
-    console.log(err)
+  if (response.status === 201) {
+    toast.success(data.message)
+  } else {
+    toast.error(data.message)
+  }
+
+  return {
+    message: data.message,
+    status: response.status,
   }
 }
