5b6c30fc8230602a5d737ec001ffa6a7300bdab6
Michal <michalbystryk14@wp.pl>
2023-11-16 20:35:48 +0100

    Remove unused Redux slices from store configuration

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit"
-import categorySlice from "./categorySlice"
-import detailSlice from "./detailSlice"
 import currencySlice from "./currencySlice"
 import cartSlice from "./cartSlice"
 
 export const store = configureStore({
   reducer: {
-    category: categorySlice.reducer,
-    detail: detailSlice.reducer,
     currency: currencySlice.reducer,
     cart: cartSlice.reducer,
   },
