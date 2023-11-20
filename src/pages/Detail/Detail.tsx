30f8eea6b544513b784672a3b535d0fa7e5976ad
Michal <michalbystryk14@wp.pl>
2023-11-20 20:41:14 +0100

    Add 'rates' condition to render; Update price prop in ItemDetail

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -2,24 +2,31 @@ import { useParams } from "react-router"
 import useProductsData from "../../hooks/products/useProductsData"
 import ItemDetail from "../../components/ItemDetail/ItemDetail"
 import { Product } from "../../types/product"
+import useExchangeRate from "../../hooks/exchange-rate/useExchangeRate"
+import { useAppSelector } from "../../hooks/redux"
 
 const Detail = () => {
   const itemId = useParams().id || ""
+  const currency = useAppSelector((state) => state.currency.currency)
   const { data } = useProductsData()
+  const { data: rates } = useExchangeRate()
 
-  const product = data.products.find(
+  const product = data?.products?.find(
     (product: Product) => product._id === itemId
   )
 
   return (
-    <ItemDetail
-      key={product._id}
-      id={product._id}
-      imageUrl={product.imageUrl}
-      name={product.name}
-      price={product.price}
-      description={product.description}
-    />
+    product &&
+    rates && (
+      <ItemDetail
+        key={product._id}
+        id={product._id}
+        imageUrl={product.imageUrl}
+        name={product.name}
+        price={product.price * rates[currency]}
+        description={product.description}
+      />
+    )
   )
 }
 
