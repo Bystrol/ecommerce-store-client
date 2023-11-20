d7eb4722f1ff402a2b77dcc1c22932b20fe8cc98
Michal <michalbystryk14@wp.pl>
2023-11-20 20:41:53 +0100

    Refactor checkout function to support currency conversion

--- src/util/api/checkout.ts
+++ src/util/api/checkout.ts
@@ -5,9 +5,17 @@ type CheckoutParams = {
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
   currency: string
   items: CartItem[]
+  rates: {
+    [key: string]: number
+  }
 }
 
-const checkout = async ({ setIsLoading, currency, items }: CheckoutParams) => {
+const checkout = async ({
+  setIsLoading,
+  currency,
+  items,
+  rates,
+}: CheckoutParams) => {
   setIsLoading(true)
 
   const authToken = localStorage.getItem("authToken")
@@ -17,12 +25,21 @@ const checkout = async ({ setIsLoading, currency, items }: CheckoutParams) => {
     return
   }
 
+  let updatedItems = [...items]
+
+  if (currency !== "USD") {
+    updatedItems = items.map((item) => ({
+      ...item,
+      price: +(item.price * rates[currency]).toFixed(2),
+    }))
+  }
+
   try {
     const response = await fetch(
       `${process.env.REACT_APP_API_URL}/checkout/create-checkout-session`,
       {
         method: "POST",
-        body: JSON.stringify({ currency, items }),
+        body: JSON.stringify({ currency, updatedItems }),
         headers: {
           "Content-Type": "application/json",
           Authorization: "Bearer " + authToken,
