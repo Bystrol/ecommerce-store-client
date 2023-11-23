8381cb53edd43dcd51c414f1e791e720a7f4b430
Michal <michalbystryk14@wp.pl>
2023-11-23 12:35:32 +0100

    Remove unused code

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,6 @@
 import classes from "./Home.module.css"
-import { useEffect } from "react"
-import toast from "react-hot-toast"
-import { useSearchParams } from "react-router-dom"
 
 const Home = () => {
-  let [searchParams, setSearchParams] = useSearchParams()
-  const success = searchParams.get("success")
-  const canceled = searchParams.get("canceled")
-
-  useEffect(() => {
-    if (success) {
-      toast.success("Thank you for your order!")
-      localStorage.removeItem("cart")
-    }
-    if (canceled) toast.error("Your order was not completed! Try again.")
-    setSearchParams([])
-  }, [success, canceled, setSearchParams])
-
   return (
     <div className={classes.body}>
       <p>Your favourite online clothing shop.</p>
