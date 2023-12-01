dc51c7b24702d726c077d2e3b160f1b9a0942e7f
Michal <michalbystryk14@wp.pl>
2023-12-01 17:24:45 +0100

    Refactor to use useNavigationLinks hook

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,11 +1,14 @@
 import classes from "./Navigation.module.css"
 import { NavLink } from "react-router-dom"
+import useNavigationLinks from "../../hooks/navigation/useNavigationLinks"
 
 type NavigationProps = {
   show: boolean
 }
 
 const Navigation = (props: NavigationProps) => {
+  const navLinks = useNavigationLinks()
+
   return (
     <nav
       className={`${classes.navigation} ${
@@ -13,36 +16,22 @@ const Navigation = (props: NavigationProps) => {
       }`}
     >
       <ul>
-        <li>
-          <NavLink
-            to="/category/women"
-            className={(navData) =>
-              navData.isActive ? classes.active : classes.link
-            }
-          >
-            women
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/category/men"
-            className={(navData) =>
-              navData.isActive ? classes.active : classes.link
-            }
-          >
-            men
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/category/kids"
-            className={(navData) =>
-              navData.isActive ? classes.active : classes.link
-            }
-          >
-            kids
-          </NavLink>
-        </li>
+        {navLinks.map((link, index) => {
+          return (
+            link.canBeAccessed && (
+              <li key={index}>
+                <NavLink
+                  to={link.path}
+                  className={(navData) =>
+                    navData.isActive ? classes.active : classes.link
+                  }
+                >
+                  {link.categoryName}
+                </NavLink>
+              </li>
+            )
+          )
+        })}
       </ul>
     </nav>
   )
