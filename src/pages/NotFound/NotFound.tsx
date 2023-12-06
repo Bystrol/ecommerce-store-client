3ed34c0c4979c8c129b1d927e1638f2241e97505
Michal <michalbystryk14@wp.pl>
2023-12-06 15:16:11 +0100

    Update classname

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,7 @@
 import styles from "./NotFound.module.css"
 
 const NotFound = () => {
-  return <h1 className={styles.header}>Page not found</h1>
+  return <h1 className={styles.heading}>Page not found</h1>
 }
 
 export default NotFound
