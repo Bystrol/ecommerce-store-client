a09aedd18deaaa0fb70fb1d0c4a7e47651ecce74
Michal <michalbystryk14@wp.pl>
2023-12-06 22:51:52 +0100

    Add declaration extension

---
+++ src/types/form.d.ts
@@ -0,0 +1,38 @@
+export type LoginFormData = {
+  email: string
+  password: string
+  isInvalid: {
+    email: boolean
+    password: boolean
+  }
+  inputTouched: {
+    [key: string]: boolean
+    email: boolean
+    password: boolean
+  }
+  isFormValid: boolean
+}
+
+export type RegisterFormData = LoginFormData & {
+  username: string
+  confirmPassword: string
+  isInvalid: {
+    username: boolean
+    confirmPassword: boolean
+  }
+  inputTouched: {
+    username: boolean
+    confirmPassword: boolean
+  }
+}
+
+export type UpdatedLoginInvalid = {
+  [key: string]: boolean
+  email: boolean
+  password: boolean
+}
+
+export type UpdatedRegisterInvalid = UpdatedLoginInvalid & {
+  username: boolean
+  confirmPassword: boolean
+}
---
+++ src/types/product.d.ts
@@ -0,0 +1,19 @@
+export type Product = {
+  description: string
+  _id: string
+  imageUrl: string
+  isAvailable: boolean
+  name: string
+  price: number
+  category: string
+}
+
+export type CartItem = {
+  amount: number
+  color: string
+  id: string
+  imageUrl: string
+  name: string
+  price: number
+  size: string
+}
