e9638fb934e0d757d36feb00bb337cea077ee4e1
Michal <michalbystryk14@wp.pl>
2023-12-06 15:13:36 +0100

    Update title text

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import classes from "./Home.module.css"
 const Home = () => {
   return (
     <div className={classes.body}>
-      <p>Your favourite online clothing shop.</p>
+      <p>Your favourite online clothing store.</p>
       <div className={classes.layer} />
       <div className={classes.background} />
     </div>
