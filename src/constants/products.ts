1fda82c914c667213561742150b52c990342baa7
Michal <michalbystryk14@wp.pl>
2023-12-11 13:54:13 +0100

    Add product constants for cloth and shoe sizes and colors

---
+++ src/constants/products.ts
@@ -0,0 +1,41 @@
+export const clothSizes: { size: string }[] = [
+  {
+    size: "xs",
+  },
+  {
+    size: "s",
+  },
+  {
+    size: "m",
+  },
+  {
+    size: "l",
+  },
+]
+
+export const shoeSizes: { size: string }[] = [
+  {
+    size: "41",
+  },
+  {
+    size: "42",
+  },
+  {
+    size: "43",
+  },
+  {
+    size: "44",
+  },
+]
+
+export const colors: { color: string }[] = [
+  {
+    color: "brown",
+  },
+  {
+    color: "gray",
+  },
+  {
+    color: "black",
+  },
+]
